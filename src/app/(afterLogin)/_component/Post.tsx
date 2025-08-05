f8ca1e60c5151ed3c6c11580ca7a34e1750a30e4
sunmi An <aseonmi220@gmail.com>
2025-08-05 23:51:23 +0900

    feat: 홈 ui 완성

--- src/app/(afterLogin)/_component/Post.tsx
+++ src/app/(afterLogin)/_component/Post.tsx
@@ -1,7 +1,50 @@
+import Link from 'next/link';
+import style from './post.module.css'
+import dayjs from 'dayjs'
+import relativeTime from 'dayjs/plugin/relativeTime'
+import 'dayjs/locale/ko'
+import ActionButtons from './ActionButtons'
+
+dayjs.extend(relativeTime)
+dayjs.locale('ko')
+
 export default function Post() {
-    return (
-        <div>폼
-            
+    const target = {
+        User : {
+            id : 'elonmusk',
+            nickname : 'Elon Musk',
+            image : './yRsRRjGO.jpg'
+        },
+        content : "클론코딩 라이브로 하니 너무 힘들어요 ㅠㅠ",
+        createdAt : new Date(),
+        Images : []
+    }
+  return (
+    <article className={style.post}>
+      <div className={style.postWrapper}>
+        <div className={style.postUserSection}>
+          <Link href={`/${target.User.id}`} className={style.postUserImage}>
+            <img src={target.User.image} alt={target.User.nickname} />
+            <div className={style.postShade} />
+          </Link>
         </div>
-    )
+        <div className={style.postBody}>
+          <div className={style.postMeta}>
+            <Link href={`/${target.User.id}`}>
+              <span className={style.postUserName}>{target.User.nickname}</span>
+              &nbsp;
+              <span className={style.postUserId}>@{target.User.id}</span>
+              &nbsp; · &nbsp;
+            </Link>
+            <span className={style.postDate}>
+              {dayjs(target.createdAt).fromNow(true)}
+            </span>
+          </div>
+          <div>{target.content}</div>
+          <div className={style.postImageSection}></div>
+          <ActionButtons />
+        </div>
+      </div>
+    </article>
+  );
 }
