885706dd3741bfa3d0aac4714eb6088697b4d251
sunmi An <aseonmi220@gmail.com>
2025-08-06 23:02:13 +0900

    feat: explore 페이지 ui 완성

--- src/app/(afterLogin)/_component/TrendSection.tsx
+++ src/app/(afterLogin)/_component/TrendSection.tsx
@@ -1,7 +1,16 @@
+"use client";
+
 import styles from "./trendSection.module.css";
 import Trend from "./Trend";
+import { usePathname } from "next/navigation";
 
 export default function TrendSection() {
+    const pathname = usePathname();
+
+    if (pathname === "/explore") {
+        return null;
+    }
+    
     return (
         <>
         <div className={styles.trendBg}>
--- src/app/(afterLogin)/explore/page.tsx
+++ src/app/(afterLogin)/explore/page.tsx
@@ -1,7 +1,25 @@
+import SearchForm from "../_component/SearchForm";
+import Trend from "../_component/Trend";
+import styles from './explore.module.css'
+
 export default function Explore() {
-    return (
-        <>
-        <h1>탐색하기</h1>
-        </>
-    )
+  return (
+    <main className={styles.main}>
+      <div className={styles.formZone}>
+        <SearchForm />
+      </div>
+      <div className={styles.trend}>
+            <h3>나를 위한 트랜드</h3>
+            <Trend />
+            <Trend />
+            <Trend />
+            <Trend />
+            <Trend />
+            <Trend />
+            <Trend />
+            <Trend />
+            <Trend />
+      </div>
+    </main>
+  );
 }
